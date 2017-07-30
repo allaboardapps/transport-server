80e2fd838f0819a95d2b6df62a2dd64fc34549b5
Randy Burgess <wrburgess@gmail.com>
2017-07-30 15:04:11 -0500

    aa loading error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
+  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
