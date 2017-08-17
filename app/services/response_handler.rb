7bed4049986650391c8406a1a77d8951d1f6f28a
Randy Burgess <wrburgess@gmail.com>
2017-08-17 17:19:38 -0500

    switching to station id alone

--- app/services/response_handler.rb
+++ app/services/response_handler.rb
@@ -4,18 +4,18 @@ class ResponseHandler
 
     if intent == Intents::STATION_DIRECT
       station_id = conn[:request][:slots][:station_id]
-      direction = conn[:request][:slots][:direction]
+      # direction = conn[:request][:slots][:direction]
 
       if !Station.stopid_valid?(stopid: station_id)
         conn[:response][:ssml] = "<speak>Which station i.d.?</speak>"
         conn[:response][:should_end_session] = false
         conn[:response][:slot_to_elicit] = Slots::STATION_ID
         conn[:response][:template] = "dialog"
-      elsif !Direction.valid?(name: direction)
-        conn[:response][:ssml] = "<speak>Which direction?</speak>"
-        conn[:response][:should_end_session] = false
-        conn[:response][:slot_to_elicit] = Slots::DIRECTION
-        conn[:response][:template] = "dialog"
+      # elsif !Direction.valid?(name: direction)
+      #   conn[:response][:ssml] = "<speak>Which direction?</speak>"
+      #   conn[:response][:should_end_session] = false
+      #   conn[:response][:slot_to_elicit] = Slots::DIRECTION
+      #   conn[:response][:template] = "dialog"
       else
         conn[:response][:ssml] = "<speak>We're all good</speak>"
         conn[:response][:should_end_session] = true
