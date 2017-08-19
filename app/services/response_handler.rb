eedfc1b56a69eabee69cd9e615cd00d18b884d8d
Randy Burgess <wrburgess@gmail.com>
2017-08-18 19:19:18 -0500

    updating dialog control

--- app/services/response_handler.rb
+++ app/services/response_handler.rb
@@ -4,21 +4,26 @@ class ResponseHandler
 
     if intent == Intents::STATION_DIRECT
       station_id = conn[:request][:slots][:station_id]
-      # direction = conn[:request][:slots][:direction]
+      direction = conn[:request][:slots][:direction]
 
-      if !Station.stopid_valid?(stopid: station_id)
+      if Station.stopid_valid?(stopid: station_id)
+        conn[:response][:slots][:station_id] = station_id
+      end
+
+      if Direction.valid?(name: direction)
+        conn[:response][:slots][:direction] = direction
+      end
+
+      if conn[:response][:slots][:station_id].blank?
         conn[:response][:ssml] = "<speak>Which station i.d.?</speak>"
-        conn[:response][:should_end_session] = false
         conn[:response][:slot_to_elicit] = Slots::STATION_ID
         conn[:response][:template] = "dialog"
-      # elsif !Direction.valid?(name: direction)
-      #   conn[:response][:ssml] = "<speak>Which direction?</speak>"
-      #   conn[:response][:should_end_session] = false
-      #   conn[:response][:slot_to_elicit] = Slots::DIRECTION
-      #   conn[:response][:template] = "dialog"
+      elsif conn[:response][:slots][:direction].blank?
+        conn[:response][:ssml] = "<speak>Which direction?</speak>"
+        conn[:response][:slot_to_elicit] = Slots::DIRECTION
+        conn[:response][:template] = "dialog"
       else
         conn[:response][:ssml] = "<speak>We're all good</speak>"
-        conn[:response][:should_end_session] = true
         conn[:response][:template] = "completed"
       end
     elsif intent == Intents::NEXT_TRAIN
@@ -60,7 +65,6 @@ class ResponseHandler
     end
 
     session_log = SessionLog.find_by(id: conn[:session_log_id])
-
     session_log.update(
       ssml: conn[:response][:ssml],
       slot_to_elicit: conn[:response][:slot_to_elicit],
